7819b1f31ef3110a9b7ae2d1619bedf2052e27aa
bhawana-web <bhawnav161102@gmail.com>
2024-12-20 21:16:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;  // Private variable for width
+    this._height = height;  // Private variable for height
+  }
 
-class Square extends Animal {}
+  // Getter for width
+  get width() {
+    return this._width;
+  }
 
-// Do not change the code below this line
-window.Rectangle = Rectangle;
-window.Square = Square;
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate area of the rectangle
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+// Square class extends Rectangle
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);  // A square is a rectangle with equal width and height
+  }
+
+  // Method to calculate perimeter of the square
+  getPerimeter() {
+    return 4 * this.width;  // Since all sides are equal, we can use width or height
+  }
+}
